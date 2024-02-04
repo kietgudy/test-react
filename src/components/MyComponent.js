16fed2f4f8b6adcfd08c19972fb66e39c36d1fab
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-02-05 00:51:18 +0700

    video 20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,11 +5,33 @@ class MyComponent extends React.Component {
     address: "Quang Ngai",
     age: 23,
   };
+
+  handleClick = (event) => {
+    console.log("Click me");
+
+    //Merge state => React Class
+    this.setState({
+      name: "Kiet Nguyen",
+      age: Math.floor(Math.random() * 100) + 1,
+    });
+  };
+  
+  handleOnMouseOver(event) {
+    // console.log(event);
+  }
   //JSX
   render() {
     return (
       <div>
-        My Name is {this.state.name} and from {this.state.address}
+        My Name is {this.state.name} and I'm {this.state.age}
+        <button
+          onClick={(event) => {
+            this.handleClick(event);
+          }}
+        >
+          Click me
+        </button>
+        <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
       </div>
     );
   }
