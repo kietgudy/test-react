54d170859101ec53de1cfa5224d76d61ac8af8a0
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-02-04 18:12:30 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+const App = () => {
+  const count = useSelector(state => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>Count = {count}</div>
+        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
