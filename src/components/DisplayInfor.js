1f351f92858f033f40158339b8a9e783e0f0ba27
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-02-09 18:17:57 +0700

    video 37 hooks

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,49 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DisplayInfor.scss";
+import logo from "./../logo.svg";
 
-class DisplayInfor extends React.Component {
-  state = {
-    isShowListUsers: true,
-  };
-  handleShowHide = () => {
-    this.setState({
-      isShowListUsers: !this.state.isShowListUsers,
-    });
-  };
-  render() {
-    //Destructuring
-    const { listUsers } = this.props;
-    //Props
-    return (
-      <div className="display-infor-container">
-        <div>
-          <span
-            onClick={() => {
-              this.handleShowHide();
-            }}
-          >
-            {this.state.isShowListUsers === true
-              ? "Hide list users: "
-              : "Show list users:"}
-          </span>
-        </div>
+// class DisplayInfor extends React.Component {
+//   render() {
+//     //Destructuring
+//     const { listUsers } = this.props;
+//     //Props
+//     return (
+//       <div className="display-infor-container">
+//         {true && (
+//           <div>
+//             {listUsers.map((user) => {
+//               return (
+//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                   <div>My name's {user.name}</div>
+//                   <div>My age's {user.age}</div>
+//                   <div>
+//                     <button
+//                       onClick={() => this.props.handleDeleteUser(user.id)}
+//                     >
+//                       Delete
+//                     </button>
+//                   </div>
+//                   <hr></hr>
+//                 </div>
+//               );
+//             })}
+//           </div>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
-        {this.state.isShowListUsers && (
-          <div>
-            {listUsers.map((user) => {
-              return (
-                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                  <div>My name's {user.name}</div>
-                  <div>My age's {user.age}</div>
-                  <hr></hr>
-                </div>
-              );
-            })}
-          </div>
-        )}
+const DisplayInfor = (props) => {
+  const { listUsers } = props;
+  const [isShowHideListUser, setShowHideListUser] = useState(true);
+  const handleShowHideListUser = () => {
+    setShowHideListUser(!isShowHideListUser);
+  };
+  return (
+    <div className="display-infor-container">
+      <div>
+        <span onClick={() => handleShowHideListUser()}>
+          {isShowHideListUser === true ? "Hide list users" : "Show list users"}
+        </span>
       </div>
-    );
-  }
-}
+      {isShowHideListUser && (
+        <div>
+          {listUsers.map((user) => {
+            return (
+              <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                <div>My name's {user.name}</div>
+                <div>My age's {user.age}</div>
+                <div>
+                  <button onClick={() => props.handleDeleteUser(user.id)}>
+                    Delete
+                  </button>
+                </div>
+                <hr></hr>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default DisplayInfor;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,31 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUserInfor from "./AddUserInfor";
 import DisplayInfor from "./DisplayInfor";
-class MyComponent extends React.Component {
-  state = {
-    listUsers: [
-      { id: 1, name: "Kiet Gody", age: "16" },
-      { id: 2, name: "Kiet Gody 2", age: "20" },
-      { id: 3, name: "Kiet Gody 3", age: "36" },
-    ],
-  };
+// class MyComponent extends React.Component {
+//   state = {
+//     listUsers: [
+//       { id: 1, name: "Kiet Gody", age: "16" },
+//       { id: 2, name: "Kiet Gody 2", age: "20" },
+//       { id: 3, name: "Kiet Gody 3", age: "36" },
+//     ],
+//   };
 
-  handleAddNewUser = (userOjb) => {
-    console.log(userOjb);
-    this.setState({
-      listUsers: [userOjb, ...this.state.listUsers],
-    });
+//   handleAddNewUser = (userOjb) => {
+//     console.log(userOjb);
+//     this.setState({
+//       listUsers: [userOjb, ...this.state.listUsers],
+//     });
+//   };
+//   handleDeleteUser = (userId) => {
+//     let listUsersClone = this.state.listUsers;
+//     listUsersClone = listUsersClone.filter((item) => item.id !== userId);
+//     this.setState({ listUsers: listUsersClone });
+//   };
+//   //JSX
+//   render() {
+//     return (
+//       <div>
+//         <AddUserInfor handleAddNewUser={this.handleAddNewUser} />
+//         <br />
+//         <br />
+//         <DisplayInfor listUsers={this.state.listUsers} handleDeleteUser={this.handleDeleteUser}/>
+//       </div>
+//     );
+//   }
+// }
+const MyComponent = (props) => {
+  const [listUsers, setListUsers] = useState([
+    { id: 1, name: "Kiet Gody", age: "16" },
+    { id: 2, name: "Kiet Gody 2", age: "20" },
+    { id: 3, name: "Kiet Gody 3", age: "36" },
+  ]);
+  const handleAddNewUser = (userOjb) => {
+    setListUsers([userOjb, ...listUsers]);
+    // this.setState({
+    //   listUsers: [userOjb, ...this.state.listUsers],
+    // });
   };
-  //JSX
-  render() {
-    return (
-      <div>
-        <AddUserInfor handleAddNewUser={this.handleAddNewUser} />
-        <br />
-        <br />
-        <DisplayInfor listUsers={this.state.listUsers} />
-      </div>
-    );
-  }
-}
+  const handleDeleteUser = (userId) => {
+    let listUsersClone = listUsers;
+    listUsersClone = listUsersClone.filter((item) => item.id !== userId);
+    setListUsers(listUsersClone);
+    // this.setState({ listUsers: listUsersClone });
+  };
+  return (
+    <div>
+      <AddUserInfor handleAddNewUser={handleAddNewUser} />
+      <br />
+      <br />
+      <DisplayInfor listUsers={listUsers} handleDeleteUser={handleDeleteUser} />
+    </div>
+  );
+};
 export default MyComponent;
