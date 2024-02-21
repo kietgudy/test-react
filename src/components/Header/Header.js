17394aca826b5f4eb3fa5ef767dca184696b8f01
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-02-21 21:06:26 +0700

    video53

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,11 +25,13 @@ const Header = () => {
             </NavLink>
           </Nav>
           <Nav>
-            <NavDropdown title="Settings" id="basic-nav-dropdown">
+            <button className="btn-login">Log in</button>
+            <button className="btn-signup">Sign up</button>
+            {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
               <NavDropdown.Item>Log in</NavDropdown.Item>
               <NavDropdown.Item>Log out</NavDropdown.Item>
               <NavDropdown.Item>Profile</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,4 +1,25 @@
+import videoHomePage from "../../assets/video-homepage.mp4";
 const HomePage = (props) => {
-  return <div className="homepage-container">Home page</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay muted loop>
+        <source src={videoHomePage} type="video/mp4" />
+      </video>
+      <div className="homepage-content">
+        <div>
+          <h1 className="title-intro">Make forms worth filling out</h1>
+        </div>
+        <div className="title-body">
+          <span>
+            Get more data-like signups, feedback, and anything else-with forms
+            designed to be refreshingly different.
+          </span>
+        </div>
+        <div >
+          <button className="btn-start">Get started-it's free</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default HomePage;
